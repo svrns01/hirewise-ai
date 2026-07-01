ae498137c8d1ea6670505ed4b8670c43e6039bb0
Venkat <venkatnaralas@outlook.com>
2026-07-01 22:16:16 +0530

    Restructuring backend

---
+++ backend/src/main/java/com/leege/hirewise/api/HirewiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.leege.hirewise.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirewiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirewiseApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/leege/hirewise/api/HirewiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leege.hirewise.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirewiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
