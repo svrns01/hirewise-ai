6d5e10e1090f31570c80c6b4a048f2fdd109ae2d
Venkat <saktivenkatrajns@gmail.com>
2026-07-01 23:07:01 +0530

    day1 backend accomplished

--- backend/src/main/java/com/leege/hirewise/api/controller/HomeController.java
+++ backend/src/main/java/com/leege/hirewise/api/controller/HomeController.java
@@ -1,4 +1,15 @@
 package com.leege.hirewise.api.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class HomeController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
 }
